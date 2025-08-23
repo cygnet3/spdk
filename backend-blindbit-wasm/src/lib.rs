224c740ac3f5977c9535dcc40dadf958069f1e9c
Sosthene <contact@sosthene.net>
2025-08-23 13:14:24 +0200

    Reexport wasm_bindgen_futures from wasm package

--- backend-blindbit-wasm/src/lib.rs
+++ backend-blindbit-wasm/src/lib.rs
@@ -30,5 +30,6 @@ mod scanner;
 pub use backend::{ChainBackend, BlindbitBackend, BlindbitClient};
 pub use scanner::SpScanner;
 
-// Re-export core client for convenience (includes Updater)
-pub use sp_client::*;
+use sp_client::*;
+
+pub use wasm_bindgen_futures;
