9d6fff5a1625db2078dc5e50e2da6bfd92f446a2
cygnet <cygnet3@proton.me>
2025-11-06 21:37:28 +0100

    Combine separate iterations

--- src/client/client.rs
+++ src/client/client.rs
@@ -122,20 +122,9 @@ impl SpClient {
         #[cfg(not(feature = "rayon"))]
         let tweak_data_iterator = tweak_data_vec.into_iter();
 
-        let shared_secrets: Vec<PublicKey> = tweak_data_iterator
-            .map(|tweak| sp_utils::receiving::calculate_ecdh_shared_secret(&tweak, b_scan))
-            .collect();
-
-        // parallel iterator using rayon
-        #[cfg(feature = "rayon")]
-        let shared_secrets_iterator = shared_secrets.into_par_iter();
-
-        // regular iterator
-        #[cfg(not(feature = "rayon"))]
-        let shared_secrets_iterator = shared_secrets.into_iter();
-
-        let items: Result<Vec<_>> = shared_secrets_iterator
-            .map(|secret| {
+        let items: Result<Vec<_>> = tweak_data_iterator
+            .map(|tweak| {
+                let secret = sp_utils::receiving::calculate_ecdh_shared_secret(&tweak, b_scan);
                 let spks = self.sp_receiver.get_spks_from_shared_secret(&secret)?;
 
                 Ok((secret, spks.into_values()))
