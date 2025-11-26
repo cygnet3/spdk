ae3ed5eaa7a3ec8bf9d5a3e35586d07f856c4e7f
pythcoiner <pythcoiner@proton.me>
2025-11-26 06:43:55 +0100

    blindbit-native-non-async: drop url dependency

--- backend-blindbit-native-non-async/src/client/client.rs
+++ backend-blindbit-native-non-async/src/client/client.rs
@@ -1,5 +1,4 @@
 use bitcoin::{absolute::Height, secp256k1::PublicKey, Amount, Txid};
-use url::Url;
 
 use anyhow::Result;
 
@@ -17,7 +16,7 @@ use super::structs::{
 #[derive(Clone)]
 pub struct BlindbitClient<H: HttpClient> {
     http_client: H,
-    host_url: Url,
+    host_url: String,
 }
 
 impl<H: HttpClient> BlindbitClient<H> {
@@ -26,12 +25,10 @@ impl<H: HttpClient> BlindbitClient<H> {
     /// # Arguments
     /// * `host_url` - Base URL of the Blindbit server
     /// * `http_client` - HTTP client implementation
-    pub fn new(host_url: String, http_client: H) -> Result<Self> {
-        let mut host_url = Url::parse(&host_url)?;
-
+    pub fn new(mut host_url: String, http_client: H) -> Result<Self> {
         // we need a trailing slash, if not present we append it
-        if !host_url.path().ends_with('/') {
-            host_url.set_path(&format!("{}/", host_url.path()));
+        if !host_url.ends_with('/') {
+            host_url = format!("{}/", host_url);
         }
 
         Ok(BlindbitClient {
@@ -41,14 +38,14 @@ impl<H: HttpClient> BlindbitClient<H> {
     }
 
     pub fn block_height(&self) -> Result<Height> {
-        let url = self.host_url.join("block-height")?;
+        let url = join(&self.host_url, "block-height");
         let body = self.http_client.get(url.as_str(), &[])?;
         let blkheight: BlockHeightResponse = serde_json::from_str(&body)?;
         Ok(blkheight.block_height)
     }
 
     pub fn tweaks(&self, block_height: Height, dust_limit: Amount) -> Result<Vec<PublicKey>> {
-        let url = self.host_url.join(&format!("tweaks/{}", block_height))?;
+        let url = join(&self.host_url, &format!("tweaks/{}", block_height));
         let body = self.http_client.get(
             url.as_str(),
             &[("dustLimit", dust_limit.to_sat().to_string())],
@@ -57,9 +54,7 @@ impl<H: HttpClient> BlindbitClient<H> {
     }
 
     pub fn tweak_index(&self, block_height: Height, dust_limit: Amount) -> Result<Vec<PublicKey>> {
-        let url = self
-            .host_url
-            .join(&format!("tweak-index/{}", block_height))?;
+        let url = join(&self.host_url, &format!("tweak-index/{}", block_height));
         let body = self.http_client.get(
             url.as_str(),
             &[("dustLimit", dust_limit.to_sat().to_string())],
@@ -68,37 +63,34 @@ impl<H: HttpClient> BlindbitClient<H> {
     }
 
     pub fn utxos(&self, block_height: Height) -> Result<Vec<UtxoResponse>> {
-        let url = self.host_url.join(&format!("utxos/{}", block_height))?;
+        let url = join(&self.host_url, &format!("utxos/{}", block_height));
         let body = self.http_client.get(url.as_str(), &[])?;
         Ok(serde_json::from_str(&body)?)
     }
 
     pub fn spent_index(&self, block_height: Height) -> Result<SpentIndexResponse> {
-        let url = self
-            .host_url
-            .join(&format!("spent-index/{}", block_height))?;
+        let url = join(&self.host_url, &format!("spent-index/{}", block_height));
         let body = self.http_client.get(url.as_str(), &[])?;
         Ok(serde_json::from_str(&body)?)
     }
 
     pub fn filter_new_utxos(&self, block_height: Height) -> Result<FilterResponse> {
-        let url = self
-            .host_url
-            .join(&format!("filter/new-utxos/{}", block_height))?;
+        let url = join(
+            &self.host_url,
+            &format!("filter/new-utxos/{}", block_height),
+        );
         let body = self.http_client.get(url.as_str(), &[])?;
         Ok(serde_json::from_str(&body)?)
     }
 
     pub fn filter_spent(&self, block_height: Height) -> Result<FilterResponse> {
-        let url = self
-            .host_url
-            .join(&format!("filter/spent/{}", block_height))?;
+        let url = join(&self.host_url, &format!("filter/spent/{}", block_height));
         let body = self.http_client.get(url.as_str(), &[])?;
         Ok(serde_json::from_str(&body)?)
     }
 
     pub fn forward_tx(&self, tx_hex: String) -> Result<Txid> {
-        let url = self.host_url.join("forward-tx")?;
+        let url = join(&self.host_url, "forward-tx");
         let request = ForwardTxRequest::new(tx_hex);
         let json_body = serde_json::to_string(&request)?;
         let body = self.http_client.post_json(url.as_str(), &json_body)?;
@@ -106,8 +98,12 @@ impl<H: HttpClient> BlindbitClient<H> {
     }
 
     pub fn info(&self) -> Result<InfoResponse> {
-        let url = self.host_url.join("info")?;
+        let url = join(&self.host_url, "info");
         let body = self.http_client.get(url.as_str(), &[])?;
         Ok(serde_json::from_str(&body)?)
     }
+}
+
+fn join(url: &str, route: &str) -> String {
+    format!("{url}/{route}")
 }
