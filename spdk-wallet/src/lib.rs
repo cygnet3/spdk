0df609846104212f60e24abced3059a72ec0bfad
cygnet <cygnet3@proton.me>
2026-02-20 00:38:05 +0100

    feat: add 'backend-blindbit-v1' feature to spdk-wallet
    Add a feature to add the blindbit backend by default. This way consumer
    (like dana) only need to add spdk-wallet as a dependency.

--- spdk-wallet/src/lib.rs
+++ spdk-wallet/src/lib.rs
@@ -5,6 +5,10 @@ pub mod scanner;
 pub use spdk_core::chain;
 pub use spdk_core::updater;
 
+// re-export blindbit backend if enabled
+#[cfg(feature = "backend-blindbit-v1")]
+pub use backend_blindbit_v1;
+
 // re-export libraries for consumers
 pub use bitcoin;
 pub use silentpayments;
