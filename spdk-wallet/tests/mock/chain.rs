44f5c20342fdc109f9187509beb33446d0484193
cygnet <cygnet3@proton.me>
2026-03-11 14:41:33 +0100

    test: add mock chain backend and updater
    Add a chain backend that reads from files, and a chain updater that
    simply stores all updates in memory.

---
+++ spdk-wallet/tests/mock/chain.rs
@@ -0,0 +1,64 @@
+use anyhow::Result;
+use backend_blindbit_v1::api_structs::{FilterResponse, SpentIndexResponse, UtxoResponse};
+use std::{fs::File, ops::RangeInclusive, pin::Pin};
+
+use async_trait::async_trait;
+use bitcoin::{Amount, absolute::Height, secp256k1::PublicKey};
+use futures::{Stream, stream};
+
+use spdk_core::chain::{BlockData, ChainBackend, SpentIndexData, UtxoData};
+
+const BLOCK_DATA_PATH: &str = "tests/resources/blocks";
+
+pub struct MockChainBackend {}
+
+#[async_trait]
+impl ChainBackend for MockChainBackend {
+    fn get_block_data_for_range(
+        &self,
+        range: RangeInclusive<u32>,
+        _dust_limit: Amount,
+        _with_cutthrough: bool,
+    ) -> Pin<Box<dyn Stream<Item = Result<BlockData>> + Send>> {
+        let values = range.map(move |n| {
+            let file = File::open(format!("{BLOCK_DATA_PATH}/{n}/tweaks.json")).unwrap();
+            let tweaks: Vec<PublicKey> = serde_json::from_reader(file).unwrap();
+
+            let file = File::open(format!("{BLOCK_DATA_PATH}/{n}/filter-new-utxos.json")).unwrap();
+            let new_utxo_filter: FilterResponse = serde_json::from_reader(file).unwrap();
+
+            let file = File::open(format!("{BLOCK_DATA_PATH}/{n}/filter-spent.json")).unwrap();
+            let spent_filter: FilterResponse = serde_json::from_reader(file).unwrap();
+
+            let blkhash = new_utxo_filter.block_hash;
+            let blkheight = new_utxo_filter.block_height;
+
+            Ok(BlockData {
+                blkheight,
+                blkhash,
+                tweaks,
+                new_utxo_filter: new_utxo_filter.into(),
+                spent_filter: spent_filter.into(),
+            })
+        });
+
+        let stream = stream::iter(values);
+
+        Box::pin(stream)
+    }
+
+    async fn spent_index(&self, block_height: Height) -> Result<SpentIndexData> {
+        let file =
+            File::open(format!("{BLOCK_DATA_PATH}/{block_height}/spent-index.json")).unwrap();
+        let spent_index: SpentIndexResponse = serde_json::from_reader(file).unwrap();
+
+        Ok(spent_index.into())
+    }
+
+    async fn utxos(&self, block_height: Height) -> Result<Vec<UtxoData>> {
+        let file = File::open(format!("{BLOCK_DATA_PATH}/{block_height}/utxos.json")).unwrap();
+        let utxos: Vec<UtxoResponse> = serde_json::from_reader(file).unwrap();
+
+        Ok(utxos.into_iter().map(Into::into).collect())
+    }
+}
