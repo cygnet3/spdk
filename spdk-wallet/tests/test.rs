072219af3033d59da0aed6b79733bc3d3990b1fe
cygnet <cygnet3@proton.me>
2026-03-11 17:14:37 +0100

    test: add single discovered output detection test

--- spdk-wallet/tests/test.rs
+++ spdk-wallet/tests/test.rs
@@ -2,8 +2,10 @@ use std::collections::HashSet;
 use std::sync::atomic::AtomicBool;
 
 use bitcoin::absolute::Height;
-use bitcoin::secp256k1::SecretKey;
-use bitcoin::{Amount, BlockHash, Network};
+use bitcoin::hex::FromHex;
+use bitcoin::secp256k1::{Scalar, SecretKey};
+use bitcoin::{Amount, BlockHash, Network, OutPoint, ScriptBuf};
+use silentpayments::receiving::Label;
 use spdk_wallet::client::{SpClient, SpendKey};
 use spdk_wallet::scanner::SpScanner;
 
@@ -45,7 +47,7 @@ async fn simple_scan_single_block() {
         .unwrap();
 
     scanner
-        .scan_blocks(block_height, block_height, Amount::from_sat(546), true)
+        .scan_blocks(block_height, block_height, DUST_LIMIT, true)
         .await
         .unwrap();
 
@@ -116,4 +118,76 @@ async fn simple_scan_multiple_blocks() {
     // the second update relates to the second block
     assert_eq!(updates[1].blkheight, second_block_height);
     assert_eq!(updates[1].blkhash, second_block_hash);
+}
+
+#[tokio::test]
+async fn scan_single_block_with_output() {
+    let expected_outpoint: OutPoint =
+        "93a9b81f81244f8e6be29d8d6b0a9dbe6d6de6d2d4b018001ebf855bc870be88:0"
+            .parse()
+            .unwrap();
+
+    let expected_tweak = Scalar::from_be_bytes(
+        Vec::from_hex("78258954dccdba6597729dab70068bc4353ebd046f7156d9a3f8db8438b62aa5")
+            .unwrap()
+            .try_into()
+            .unwrap(),
+    )
+    .unwrap();
+
+    let expected_label: Option<Label> = None;
+    let expected_value = Amount::from_sat(10000);
+
+    // OP_PUSHNUM_1 OP_PUSHBYTES_32 dbd93fdd869e3522405749a594c2e3f4833ac98d0f4e70da6e7294f6623258c3
+    let expected_script =
+        ScriptBuf::from_hex("5120dbd93fdd869e3522405749a594c2e3f4833ac98d0f4e70da6e7294f6623258c3")
+            .unwrap();
+
+    let mock_backend = MockChainBackend {};
+
+    let mock_update = MockUpdater::default();
+    let updates = mock_update.updates.clone();
+
+    let scan_sk = SecretKey::from_slice(&[0x01; 32]).unwrap();
+    let spend_sk = SecretKey::from_slice(&[0x02; 32]).unwrap();
+    let spend_key = SpendKey::Secret(spend_sk);
+
+    let network = Network::Signet;
+    let keep_scanning = AtomicBool::new(true);
+    let owned_outpoints = HashSet::new();
+
+    let client = SpClient::new(scan_sk, spend_key, network).unwrap();
+
+    let mut scanner = SpScanner::new(
+        client,
+        Box::new(mock_update),
+        Box::new(mock_backend),
+        owned_outpoints,
+        &keep_scanning,
+    );
+
+    let block_height: Height = Height::from_consensus(295125).unwrap();
+
+    scanner
+        .scan_blocks(block_height, block_height, DUST_LIMIT, true)
+        .await
+        .unwrap();
+
+    let updates = updates.lock().unwrap();
+
+    // first assert that we received exactly 1 update for this block
+    assert!(updates.len() == 1);
+
+    // update should contain a single output for this key pair
+    let discovered_outputs = &updates[0].discovered_outputs;
+    assert_eq!(discovered_outputs.len(), 1);
+
+    let discovered_output = discovered_outputs.get(&expected_outpoint);
+
+    assert!(discovered_output.is_some());
+
+    assert_eq!(discovered_output.unwrap().script_pubkey, expected_script);
+    assert_eq!(discovered_output.unwrap().tweak, expected_tweak);
+    assert_eq!(discovered_output.unwrap().value, expected_value);
+    assert_eq!(discovered_output.unwrap().label, expected_label);
 }
