206c6f28c5272064dc2b7410bdb1cf229e978275
Sosthene <contact@sosthene.net>
2026-08-11 13:01:34 +0200

    refactor: stop re-exporting coin_select internals

--- spdk-wallet/src/client/spend.rs
+++ spdk-wallet/src/client/spend.rs
@@ -18,10 +18,12 @@ use silentpayments::{Network as SpNetwork, SilentPaymentAddress};
 use spdk_core::constants::DATA_CARRIER_SIZE;
 use spdk_core::updater::DiscoveredOutput;
 
-use super::coin_select::{pick_utxos_for_fee_rate, select_all_utxos_for_fee_rate};
+use super::coin_select::{
+    UtxoCandidate, pick_utxos_for_fee_rate, select_all_utxos_for_fee_rate,
+};
 use super::{
     FeeRate, InputSelection, Recipient, RecipientAddress, SilentPaymentUnsignedTransaction,
-    SpClient, Strategy, UtxoCandidate,
+    SpClient, Strategy,
 };
 
 fn sp_network_from_network(network: Network) -> SpNetwork {
