92e45ccf3a1d2e84e457833137d540061ff9cdf2
Sosthene <contact@sosthene.net>
2026-09-17 11:01:06 +0200

    fix: Remove `Serialize/Deserialize` macros for `SpClient`

--- spdk-wallet/src/client/client.rs
+++ spdk-wallet/src/client/client.rs
@@ -4,7 +4,6 @@ use bitcoin::{
     Network, XOnlyPublicKey,
     secp256k1::{PublicKey, Secp256k1, SecretKey},
 };
-use serde::{Deserialize, Serialize};
 use silentpayments::{Network as SpNetwork, SharedSecret, SilentPaymentCode, SpVersion};
 use silentpayments::{bitcoin_hashes::Hash, utils as sp_utils};
 use silentpayments::{
@@ -16,7 +15,7 @@ use anyhow::{Error, Result};
 
 use super::SpendKey;
 
-#[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct SpClient {
     scan_sk: SecretKey,
     spend_key: SpendKey,
